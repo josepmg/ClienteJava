896801670aaf5f1e3f44e53246f4a5c113cb8c13
josepmg <josemello@id.uff.br>
2019-06-13 14:40:09 -0300

    Ajustado para pedir ao usuario qual protocola de Web Service ele deseja usar

--- src/clientejava/ClienteJava.java
+++ src/clientejava/ClienteJava.java
@@ -6,6 +6,7 @@
 package clientejava;
 
 import java.io.IOException;
+import java.util.Scanner;
 import org.apache.http.client.fluent.Request;
 
 /**
@@ -15,14 +16,51 @@ import org.apache.http.client.fluent.Request;
 public class ClienteJava {
 
     public static void main(String[] args) throws IOException {
-        String conteudo = Request
-                .Post("http://localhost:8080/carrinho/produtoService")
-                .addHeader("accept", "application/json")
-                .execute()
-                .returnContent()
-                .asString();
-
-        System.out.println(conteudo);
+        Scanner keyboard = new Scanner(System.in);
+        menu(keyboard);
+    }
+    
+    public static void menu(Scanner keyboard) 
+            throws IOException{
+        System.out.print("Deseja receber um JSON (1) ou XML(2)? ");
+        int opc = keyboard.nextInt();
+        String conteudo;
+        switch(opc){
+            case 1:
+                // Prepara o cabeçalho Rest
+                conteudo = Request
+                        // Local do serviço
+                        .Post("http://localhost:8080/carrinho/produtoService")
+                        // Define protocolo Rest
+                        .addHeader("accept", "application/json")
+                        // Executa
+                        .execute()
+                        // Retorna o conteúdo como string
+                        .returnContent()
+                        .asString();
+                // Apresenta o retorno (JSON)
+                System.out.println(conteudo);
+                break;
+            case 2:
+                // Prepara o cabeçalho SOAP
+                conteudo = Request
+                        // Local do serviço
+                        .Post("http://localhost:8080/carrinho/produtoService")
+                        // Define protocolo SOAP
+                        .addHeader("accept", "application/xml")
+                        // Executa
+                        .execute()
+                        // Retorna o conteúdo como string
+                        .returnContent()
+                        .asString();
+                // Apresenta o retorno (XML)
+                System.out.println(conteudo);
+                break;
+            default:
+                System.out.println("Digite uma opção válida");
+                menu(keyboard);
+                break;
+        }
     }
     
 }
